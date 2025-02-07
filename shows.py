450c8bbe4dbd8bade64901019ed1b2099cf218db
unknown <Leonardo Freitas>
2025-02-07 20:56:52 +0000

    CS50P

---
+++ shows.py
@@ -0,0 +1,29 @@
+SHOWS = [
+    " Avatar: the last ainbender",
+    "Ben 10",
+    "Arthur",
+    " Spongebob Squarepants",
+    "Phineas and ferb",
+    "Kim possible",
+    "Jimmy Neutron",
+    "the proud family",
+
+]
+
+def main():
+    cleaned_shows = []
+    for show in SHOWS:
+        cleaned_shows.append(show.strip().title())
+
+        
+
+
+    print( ', '.join(cleaned_shows))
+    
+
+
+
+
+
+
+main()
